08af61655b04924c9d2cedc8e23e652644efdd3c
Mathew Wymore <mlwymore@iastate.edu>
2023-05-31 13:06:45 -0500

    Added Julia Cbc and Clp, changed flask-sqlalchemy to sqlalchemy (#17)
    Co-authored-by: Mat Wymore <mlwymore@gmail.com>

--- install/install.sh
+++ install/install.sh
@@ -7,7 +7,7 @@ python3 -m jill install --confirm
 # should lock this to a commit hash
 julia -e 'using Pkg; Pkg.add(url="https://github.com/spine-tools/SpineInterface.jl.git")'
 julia -e 'using Pkg; Pkg.add(url="https://github.com/spine-tools/SpineOpt.jl.git")'
-julia -e 'using Pkg; Pkg.add(["XLSX", "DataFrames", "Distributions", "CSV", "Revise"])'
+julia -e 'using Pkg; Pkg.add(["XLSX", "DataFrames", "Distributions", "CSV", "Revise", "Cbc", "Clp"])'
 apt install libglib2.0-dev libgl-dev libxkbcommon-x11-0 -y
 apt install libegl-dev build-essential libgl1-mesa-dev libdbus-1-3 -y
 # WORKDIR /resilience_calculator/src
