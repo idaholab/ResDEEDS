c98c044e57561d3989e27cf2192e7b08180d27ce
mcutshaw <michael.cutshaw@inl.gov>
2023-06-09 11:20:48 -0600

    tests and .gitignore for venv

---
+++ src/tests/test.py
@@ -0,0 +1,14 @@
+from conftests import app, client
+
+def test_make_proj(client):
+    data = {"projNameValAdd": "testproj"}
+    response = client.post("/", data=data)
+    assert response.status == "302 FOUND"
+
+def test_submit_xlsx(client):
+    data = {"projNameValAdd": "testproj1"}
+    response = client.post("/", data=data)
+    data = {"system_spreadsheet": (open('src/tests/test.xlsx', 'rb')),}
+    response = client.post("/qualities", data=data)
+    assert response.status == "200 OK"
+    assert b"Error" not in response.data
