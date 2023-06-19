5c601cc10fc19d847ad4a23e293bba1f1d1d8d7a
mcutshaw <michael.cutshaw@inl.gov>
2023-06-19 11:48:16 -0600

    last shot for test ftm

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,16 +1,22 @@
-from conftests import app, client
+import sys
+sys.path.append("./src")
+from app import app
 
-def test_make_proj(client):
-    client.get("/")
-    data = {"projNameValAdd": "testproj"}
-    response = client.post("/", data=data)
-    assert response.status == "302 FOUND"
+app.testing = True
 
-def test_submit_xlsx(client):
-    client.get("/")
-    data = {"projNameValAdd": "testproj1"}
-    response = client.post("/", data=data)
-    data = {"system_spreadsheet": (open('src/tests/test.xlsx', 'rb')),}
-    response = client.post("/qualities", data=data)
-    assert response.status == "200 OK"
-    # assert b"Error" not in response.data
+def test_make_proj():
+    with app.test_client() as client:
+        client.get("/")
+        data = {"projNameValAdd": "testproj"}
+        response = client.post("/", data=data)
+        assert response.status == "302 FOUND"
+
+def test_submit_xlsx():
+    with app.test_client() as client:
+        client.get("/")
+        data = {"projNameValAdd": "testproj1"}
+        response = client.post("/", data=data)
+        data = {"system_spreadsheet": (open('src/tests/test.xlsx', 'rb')),}
+        response = client.post("/qualities", data=data)
+        assert response.status == "200 OK"
+        # assert b"Error" not in response.data
