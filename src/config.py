662ccb8ca5950227d46e093c2be6977fb7e75193
Zachary Priest <zachary.priest@inl.gov>
2023-06-22 11:48:42 -0600

    change realpath to dirname for running app from anywhere #7

--- src/config.py
+++ src/config.py
@@ -4,14 +4,14 @@ import logging
 import os
 
 try:
-    with open(os.path.join(os.path.realpath(__file__),"../config/config.json"), "r", encoding='utf-8') as config_file:
+    with open(os.path.join(os.path.dirname(__file__),"../config/config.json"), "r", encoding='utf-8') as config_file:
         config = json.load(config_file)
 except FileNotFoundError:
     logging.critical('Config file config/config.json not found.')
     exit(1)
 
 try:
-    with open(os.path.join(os.path.realpath(__file__),"../config/local.json"), "r", encoding='utf-8') as local_config_file:
+    with open(os.path.join(os.path.dirname(__file__),"../config/local.json"), "r", encoding='utf-8') as local_config_file:
         config.update(json.load(local_config_file))
 except FileNotFoundError:
     logging.info('Local config file config/local.json not found. Create this file to override default config options.')
