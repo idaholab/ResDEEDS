2e1683d4015a3f358e904f9f8e45f5de92de2a78
Nathan Woodruff <nathan.woodruff@inl.gov>
2023-12-20 18:01:31 -0700

    Pendulum issue

--- app/entrypoint.local.sh
+++ app/entrypoint.local.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 # Copyright 2023, Battelle Energy Alliance, LLC
 
-mv /opt/spine/spine-engine /opt/resilience_calculator/spine/
-mv /opt/spine/spine-items /opt/resilience_calculator/spine/
-mv /opt/spine/spinedb-api /opt/resilience_calculator/spine/
-mv /opt/spine/spinetoolbox /opt/resilience_calculator/spine/
-
-tail -f /dev/null
+flask run --host=0.0.0.0 --no-reload --debugger
--- app/install/julia.sh
+++ app/install/julia.sh
@@ -1,3 +1,5 @@
+julia -e 'using Pkg; Pkg.rm("SpineInterface")' || true
+julia -e 'using Pkg; Pkg.rm("SpineOpt")' || true
 julia -e 'using Pkg; Pkg.add(url="https://github.com/spine-tools/SpineInterface.jl.git")'
 julia -e 'using Pkg; Pkg.add(url="https://github.com/spine-tools/SpineOpt.jl.git")'
 julia -e 'using Pkg; Pkg.add(["XLSX", "DataFrames", "Distributions", "CSV", "Revise", "Cbc", "Clp"])'
