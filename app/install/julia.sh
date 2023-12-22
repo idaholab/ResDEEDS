7925642b3c49e6e310363be6d1727cf17516d9db
Nathan Woodruff <nathan.woodruff@inl.gov>
2023-12-21 18:28:15 -0700

    Docker efficiency, updated requirements.dev.txt

--- app/install/julia.sh
+++ app/install/julia.sh
@@ -3,3 +3,6 @@ julia -e 'using Pkg; Pkg.rm("SpineOpt")' || true
 julia -e 'using Pkg; Pkg.add(url="https://github.com/spine-tools/SpineInterface.jl.git")'
 julia -e 'using Pkg; Pkg.add(url="https://github.com/spine-tools/SpineOpt.jl.git")'
 julia -e 'using Pkg; Pkg.add(["XLSX", "DataFrames", "Distributions", "CSV", "Revise", "Cbc", "Clp"])'
+julia -e 'using Pkg Pkg.add("IJulia")'
+julia -e 'Pkg.update()'
+julia -e 'Pkg.build("IJulia")'
