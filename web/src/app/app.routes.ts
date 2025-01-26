91551bf1196d101f5987137061b000bed1061316
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2025-01-26 13:21:20 -0800

    update angular

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { DiagramComponent } from './diagram/diagram.component';
 
-export const routes: Routes = [
-    { path: "diagram", component: DiagramComponent },
-];
+export const routes: Routes = [];
