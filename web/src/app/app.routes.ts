c16db1f83efd9263c35e661a9c1b46e93a8205da
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2025-01-28 19:42:59 -0800

    add home component

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { AuthGuard } from './services/auth.guard';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'hello-world', component: HelloWorldComponent, canActivate: [AuthGuard] },
     { path: '**', redirectTo: '' }
