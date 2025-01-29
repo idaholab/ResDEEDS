12ad2f9490c75fb5008e21bbc7280378b58454a6
itsmostafa <mostafaxcodes@gmail.com>
2025-01-29 12:41:49 -0800

    update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,11 +2,13 @@
 
 echo "Stopping existing container.."
 dzdo docker stop resdeeds-web
+dzdo docker stop resdeeds-api
 
 echo "Pulling latest image.."
 dzdo docker pull ghcr.io/idaholab/resdeeds/web:latest
+dzdo docker pull ghcr.io/idaholab/resdeeds/api:latest
 
-echo "Launching latest container"
+echo "Launching latest containers"
 dzdo docker run -d --rm \
     -p 80:80 -p 443:443 \
     --name resdeeds-web \
@@ -14,5 +16,10 @@ dzdo docker run -d --rm \
     -v ./etc/nginx:/etc/nginx/conf.d \
     -v /var/log/nginx:/var/log/nginx \
     ghcr.io/idaholab/resdeeds/web:latest
+dzdo docker run -d --rm \
+    -p 5000:5000 \
+    --name resdeeds-api \
+    --env-file .env \
+    ghcr.io/idaholab/resdeeds/api:latest
 
 echo "Deployed"
