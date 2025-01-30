eb1b77685f8fe435c56f0eb3dcf5a4b132efbf5e
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2025-01-30 08:33:54 -0800

    update home component

--- web/src/app/components/home/home.component.ts
+++ web/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
