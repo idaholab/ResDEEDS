d53a8f68e0c44c3fefd09f5bd62778aadd97f7a8
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2025-02-19 09:23:49 -0800

    revert app.config.ts

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,17 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './services/auth.interceptor';
-
-import { ProjectService } from './services/project.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    ProjectService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ]
 };
