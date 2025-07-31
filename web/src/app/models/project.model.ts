6330ab39d03e9e0efe4c8f7e27603b6c8848bcdd
Mostafa Abdo <mostafaxcodes@gmail.com>
2025-07-31 15:17:40 -0700

    delete angular app

--- web/src/app/models/project.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Project {
-    _id?: string;
-    name: string;
-    created_at?: string;
-    updated_at?: string;
-}
--- web/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000/api',
-    drawioUrl: 'http://localhost:8080',
-    // URL for local Draw.io files served from Angular assets
-    drawioLocalUrl: 'assets/drawio/main/webapp/index.html',
-};
